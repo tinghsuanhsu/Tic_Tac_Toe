403429d54b3d362d6e8d17d90d228fba2a3f1cde
tinghsuanhsu <tinghsuanhsu@gmail.com>
2023-10-03 12:38:37 +1000

    Add a for loop to test checking the board for winning scenario

--- script.js
+++ script.js
@@ -11,8 +11,44 @@
 // draw
 grid = document.querySelector('.board-container');
 let boardElements = [, , , , , , , , ,];
-
+let currentPick;
+const winningArray = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+function checkWin() {
+  // this part evaluates if the user move wins
+  for (let i = 0; i < winningArray.length; i++) {
+    let test = [];
+    for (let j = 0; j < winningArray[i].length; j++) {
+      if (boardElements[winningArray[i][j]] == 1) {
+        test.push(true);
+      }
+    }
+    if (test.length > 2) {
+      let result = test.every((elem) => elem == true);
+      if (result) {
+        console.log('win');
+        console.log(winningArray[i]);
+        break;
+      }
+    }
+  }
+}
 // clear board
+function clearBoard() {
+  const gridCell = document.querySelectorAll('.board-cell');
+  [...gridCell].forEach((elem) => {
+    elem.textContent = '';
+  });
+}
+// create board
 function createBoard() {
   for (let i = 0; i < boardElements.length; i++) {
     let gridCell = document.createElement('div');
@@ -24,8 +60,6 @@ function createBoard() {
 }
 createBoard();
 function updateBoard() {
-  let userPick = 'x';
-
   // get user selection
   //
   const cell = document.querySelectorAll('.board-cell');
@@ -33,16 +67,24 @@ function updateBoard() {
     ? gridCell.classList.toggle('x')
     : gridCell.classList.toggle('o');
 }
-
 document.addEventListener('click', (e) => {
-  if (e.target.className == 'board-cell') {
-    let userPick = 'x';
-    let ind = e.target.dataset.position;
-    boardElements[ind] = 1;
-    e.target.textContent = 'x';
-    userPick == 'x'
-      ? e.target.classList.toggle('x')
-      : e.target.classList.toggle('o');
-    console.log(e.target);
+  if (e.target.className == 'player') {
+    currentPick = e.target.id;
   }
 });
+document.addEventListener('click', (e) => {
+  if (e.target.className == 'board-cell') {
+    // userPick = 'x';
+    let ind = e.target.dataset.position;
+    boardElements[ind] = 1;
+    e.target.textContent = currentPick;
+    currentPick == 'x'
+      ? e.target.classList.toggle('x')
+      : e.target.classList.toggle('o');
+    checkWin();
+  }
+});
+
+// console.log(winningArray[0]);
+// console.log(test);
+// console.log(test.every((elem) => Boolean(elem)));
